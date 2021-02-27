38b4841408aa8b4606ce0eedb01d2a439a5deff8
Damir Arh <damir.arh@gmail.com>
2021-02-27 12:16:06 +0100

    Add project files.

---
+++ EnvironmentConfiguration.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvironmentConfiguration.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
